e91328ecc9b24f7ca80960e4921f61babd1ed4c4
alaaAbdallah <alaa_abd_1999@hotmail.com>
2024-07-17 19:28:10 +0300

    bootstarp navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import Products from './components/products/Products'
+import Footer from './components/footer/Footer'
+
+export default function () {
+  return (
+    <div className='container'>
+
+<Navbar />
+<Products />
+<Footer />
+
+    </div>
+  )
+}
---
+++ src/components/products/Products.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+function Products() {
+  return (
+    <>
+<div className='row'>
+<div className='col-md-4'>
+    <div className='product'>
+    <div className="p-3 border bg-light">عمود 1</div>
+
+        <h2>product one</h2>
+        <p>this is product one</p>
+    </div>
+</div>
+
+<div className='col-md-4'>
+    <div className='product'>
+    <div className="p-3 border bg-light">عمود 2</div>
+
+        <h2>product one</h2>
+        <p>this is product one</p>
+    </div>
+</div>
+
+<div className='col-md-4'>
+<div className="p-3 border bg-light">عمود 3</div>
+    <div className='product'>
+        <h2>product one</h2>
+        <p>this is product one</p>
+    </div>
+</div>
+
+
+
+</div>
+
+
+
+
+
+
+
+   </>
+  )
+}
+
+export default Products
